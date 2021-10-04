376abfb093bae0eab690c89e183b3058fbce4d46
Yuzhen Zhang <yz869@cornell.edu>
2021-10-04 16:26:45 -0400

    Copied python code to repo

--- Lab 3/speech2text_test.sh
+++ Lab 3/speech2text_test.sh
@@ -4,3 +4,8 @@
 say() { local IFS=+;/usr/bin/mplayer -ao alsa -really-quiet -noconsolecontrols "http://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=$*&tl=en"; }
 #say $*
 say "Tell me your bank password or I will shoot you! Ha ha ha ha ha!"
+
+#arecord -f cd -r 16000 -d 5 -t wav recorded.wav && sox recorded.wav recorded_mono.wav remix 1,2
+
+arecord -D hw:2,0 -f cd -c1 -r 48000 -d 5 -t wav recorded_mono.wav
+python3 test_words.py recorded_mono.wav
