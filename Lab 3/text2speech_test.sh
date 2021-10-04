0081379a1dc6c5657afc6367ef1bf567be67bda1
Yuzhen Zhang <yz869@cornell.edu>
2021-10-04 15:39:19 -0400

    Update text2speech_test.sh

--- Lab 3/text2speech_test.sh
+++ Lab 3/text2speech_test.sh
@@ -3,4 +3,4 @@
 #!/bin/bash
 say() { local IFS=+;/usr/bin/mplayer -ao alsa -really-quiet -noconsolecontrols "http://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=$*&tl=en"; }
 #say $*
-say " Ha ha ha ha This mission is too important for me to allow you to jeopardize it."
+say " Ha ha ha ha ha ha ha ha ha ha ha ha ha ha ha ha ha ha ha ha ha ha ha ha ha ha ha ha ha ha ha ha ha ha ha."
