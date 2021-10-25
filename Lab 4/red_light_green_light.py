2a261672fe98a8479111e5a0a522301ae9d669f3
Yuzhen Zhang <yz869@cornell.edu>
2021-10-25 15:00:31 -0400

    Update red_light_green_light.py

--- Lab 4/red_light_green_light.py
+++ Lab 4/red_light_green_light.py
@@ -71,15 +71,28 @@ while True:
 
     if int(position) > int(last_position+2) and not toward_player:
         toward_player = True
+        print("last position")
+        print(last_position)
+        print("current position")
+        print(position)
+        print("current position")
+        print("int(position) > int(last_position)")
+        print(int(position) > int(last_position))
         last_position = position
-        position = -encoder.position
+        #position = -encoder.position
         print("Position: {}".format(position))
         print("head rotated towards the player")
 
-    if int(position) < int(last_position-2) and toward_player:
+    if int(position) < int(last_position) and toward_player:
         toward_player = False
+        print("last position")
+        print(last_position)
+        print("current position")
+        print(position)
+        print("int(position) < int(last_position)")
+        print(int(position) < int(last_position))
         last_position = position
-        position = -encoder.position
+        #position = -encoder.position
         print("Position: {}".format(position))
         print("head rotated towards the controller")
 
