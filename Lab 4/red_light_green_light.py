05600f79eab81a86d5c5f311f220142a6790babd
KH <liqin.he.ken@gmail.com>
2021-10-25 15:30:37 -0400

    wokring on test file - ken

--- Lab 4/red_light_green_light.py
+++ Lab 4/red_light_green_light.py
@@ -60,6 +60,33 @@ last_position = 0
 toward_player = False
 
 
+def check_movement():
+
+
+    print("VL53L1X Qwiic Test\n")
+    ToF = qwiic.QwiicVL53L1X()
+
+    if (ToF.sensor_init() == None):					 # Begin returns 0 on a good init
+        print("Sensor online!\n")
+
+    while True:
+        try:
+            ToF.start_ranging()						 # Write configuration bytes to initiate measurement
+            time.sleep(.005)
+            distance = ToF.get_distance()	 # Get the result of the measurement from the sensor
+            time.sleep(.005)
+            ToF.stop_ranging()
+
+            distanceInches = distance / 25.4
+            distanceFeet = distanceInches / 12.0
+
+            print("Distance(mm): %s Distance(ft): %s" % (distance, distanceFeet))
+
+        except Exception as e:
+            print(e)
+
+
+
 while True:
     draw.rectangle((0, 0, width, height), outline=0, fill=0)
     draw.text((x, top), "To start the game,", font=font, fill="#FFFFFF")
@@ -82,5 +109,8 @@ while True:
         position = -encoder.position
         print("Position: {}".format(position))
         print("head rotated towards the controller")
+
+
+
 
 
---
+++ Lab 4/test_ken.py
@@ -0,0 +1,51 @@
+import qwiic
+import time
+from pydub import AudioSegment
+from pydub.playback import play
+import gtts
+from io import BytesIO
+
+def eliminate_player():
+
+    text = "Bye"
+
+    tts = gtts.gTTS(text, lang='en')
+    mp3 = BytesIO()
+    tts.write_to_fp(mp3)
+    mp3.seek(0)
+    audio = AudioSegment.from_file(mp3, format='mp3')
+    play(audio)
+    gun_sound = AudioSegment.from_wav("gun_sound.wav")
+
+    play(gun_sound)
+
+
+def check_movement():
+
+    # print("VL53L1X Qwiic Test\n")
+    ToF = qwiic.QwiicVL53L1X()
+    # if (ToF.sensor_init() == None):					 # Begin returns 0 on a good init
+    #     print("Sensor online!\n")
+
+    prev_distance = None
+    sensivity = 2
+
+    while True:
+        try:
+            ToF.start_ranging()						 # Write configuration bytes to initiate measurement
+            time.sleep(.005)
+            distance = ToF.get_distance()	 # Get the result of the measurement from the sensor
+            time.sleep(.005)
+            ToF.stop_ranging()
+
+            distance= distance / 25.4/2
+
+            print("Distance(mm): %d" % (distance))
+
+        except Exception as e:
+            print(e)
+
+
+if __name__ == "__main__":
+
+    check_movement()
