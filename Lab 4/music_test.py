222180419333882daec2a893569dd22da81bf47d
KH <liqin.he.ken@gmail.com>
2021-10-25 14:33:48 -0400

    wokring on test file - ken

--- Lab 4/music_test.py
+++ Lab 4/music_test.py
@@ -1,29 +1,32 @@
 from pydub import AudioSegment
 from pydub.playback import play
+import gtts
+from io import BytesIO
 
+def eliminate_player():
 
-# def playAudio(text):
-#     print('Play Audio')
-#     print(text)
-#     tts = gtts.gTTS(text, lang='en')
-#     mp3 = BytesIO()
-#     tts.write_to_fp(mp3)
-#     mp3.seek(0)
-#     audio = AudioSegment.from_file(mp3, format='mp3')
-#     play(audio)
+    text = "Bye"
+    tts = gtts.gTTS(text, lang='en')
+    mp3 = BytesIO()
+    tts.write_to_fp(mp3)
+    mp3.seek(0)
+    audio = AudioSegment.from_file(mp3, format='mp3')
+    play(audio)
 
+    gun_sound = AudioSegment.from_wav("gun_sound.wav")
+    play(gun_sound)
 
 if __name__ == "__main__":
 
-    for i in range(10):
-
-        simida_sound = AudioSegment.from_wav("Simida.wav")
-        theme_song = AudioSegment.from_wav("ThemeSong.wav")
-        gun_sound = AudioSegment.from_wav("gun_sound.wav")
-        play(simida_sound)
-        play(theme_song)
-        play(gun_sound)
-
+    # for i in range(10):
+    #
+    #     simida_sound = AudioSegment.from_wav("Simida.wav")
+    #     theme_song = AudioSegment.from_wav("ThemeSong.wav")
+    #     gun_sound = AudioSegment.from_wav("gun_sound.wav")
+    #     play(simida_sound)
+    #     play(theme_song)
+    #     play(gun_sound)
+    eliminate_player()
 
 
 
