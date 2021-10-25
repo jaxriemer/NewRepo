b6123364156775ff30c59addb38adb8cf56dcfc3
KH <liqin.he.ken@gmail.com>
2021-10-25 14:36:06 -0400

    wokring on test file - ken

--- Lab 4/music_test.py
+++ Lab 4/music_test.py
@@ -5,7 +5,7 @@ from io import BytesIO
 
 def eliminate_player():
 
-    text = "Bye"
+    text = "Bye, annyeong"
     tts = gtts.gTTS(text, lang='en')
     mp3 = BytesIO()
     tts.write_to_fp(mp3)
@@ -19,13 +19,13 @@ def eliminate_player():
 if __name__ == "__main__":
 
     # for i in range(10):
-    #
     #     simida_sound = AudioSegment.from_wav("Simida.wav")
     #     theme_song = AudioSegment.from_wav("ThemeSong.wav")
     #     gun_sound = AudioSegment.from_wav("gun_sound.wav")
     #     play(simida_sound)
     #     play(theme_song)
     #     play(gun_sound)
+
     eliminate_player()
 
 
