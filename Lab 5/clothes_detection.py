9bfcd9bd2d011f1ac0e1cee12e866c2d1ad701e9
XinningF <xf49@cornell.edu>
2021-11-01 15:09:33 -0400

    Update clothes_detection.py

--- Lab 5/clothes_detection.py
+++ Lab 5/clothes_detection.py
@@ -60,7 +60,15 @@ for line in f.readlines():
 
 playAudio("Today's temperature is 57 degrees.")
 
+global what_to_wear
+what_to_wear = 0
+global rain
+rain = False
+global temp
+temp = -99999
+
 while(True):
+
     if webCam:
         ret, img = cap.read()
 
@@ -80,6 +88,32 @@ while(True):
     # run the inference
     prediction = model.predict(data)
     print("I think its a:",labels[np.argmax(prediction)])
+
+    if np.argmax(prediction) == 3:
+        playAudio("Background detected")
+
+    else:
+        playAudio("I think you are wearing a " + labels[np.argmax(prediction)])
+
+        if np.argmax(prediction) == what_to_wear:
+            print("Outfit matches")
+            playAudio("You are good to go. Goodbye.")
+            break
+        elif np.argmax(prediction) == 0:
+            if what_to_wear == 1:
+                playAudio("You should wear more clothes. Do not forget your coat.")
+            elif what_to_wear == 2:
+                playAudio("You should wear less clothes. Here is a t shirt.")
+        elif np.argmax(prediction) == 1:
+            if what_to_wear == 0:
+                playAudio("You should wear less clothes. Here is your jacket.")
+            elif what_to_wear == 2:
+                playAudio("You should wear less clothes. Here is a t shirt.")
+        elif np.argmax(prediction) == 2:
+            if what_to_wear == 0:
+                playAudio("You should wear more clothes. Do not forget your jacket.")
+            elif what_to_wear == 1:
+                playAudio("You should wear more clothes. Do not forget your coat.")
 
     if webCam:
         if sys.argv[-1] == "noWindow":
