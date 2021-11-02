1a0c37473bc74905e546aec449ae4cfe5c19a700
XinningF <xf49@cornell.edu>
2021-11-02 15:47:22 -0400

    part 1

--- Lab 5/playAudio.py
+++ Lab 5/playAudio.py
@@ -2,11 +2,17 @@ from gtts import gTTS
 import os
 
 # define variables
-s = "hello world"
-file = "file.mp3"
+s = "I think you are wearing a t shirt. "
+s += "You should wear more clothes. Do not forget your jacket. "
+
+s += "I think you are wearing a jacket. "
+s += "Outfit matches the weather. Goodbye. "
+
+s += "I think you are wearing a coat. "
+s += "You should wear less clothes. Here is a t shirt. "
 
 # initialize tts, create mp3 and play
 tts = gTTS(s, lang='en')
-tts.save(file)
+tts.save("audio.mp3")
 os.system("mpg123 " + file)
 
