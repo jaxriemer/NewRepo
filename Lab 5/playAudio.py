0eb5fa525597cf8a691b3593513ca226ae5901f9
XinningF <xf49@cornell.edu>
2021-11-02 14:02:07 -0400

    Update playAudio.py

--- Lab 5/playAudio.py
+++ Lab 5/playAudio.py
@@ -1,8 +1,9 @@
 from gtts import gTTS
 import os
 
-mytext = 'Test'
+mytext = 'test'
 language = 'en'
+
 myobj = gTTS(text=mytext, lang=language, slow=False)
 myobj.save("test.mp3")
-os.system("test.mp3")
+os.system("mpg321 welcome.mp3")
