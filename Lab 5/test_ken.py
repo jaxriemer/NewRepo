ea8554a9d7887b6fe879ba6cffefe10e0c2669b0
KH <liqin.he.ken@gmail.com>
2021-11-07 14:39:30 -0500

    wokring on test file - ken

--- Lab 5/test_ken.py
+++ Lab 5/test_ken.py
@@ -8,39 +8,47 @@ from adafruit_servokit import ServoKit
 kit = ServoKit(channels=16)
 
 # Name and set up the servo according to the channel you are using.
-servo_move = kit.servo[0]
-servo_pick = kit.servo[1]
+servo_slide_forward = kit.servo[0]
+servo_slide_backward = kit.servo[0]
+servo_pick = kit.servo[0]
 
 # Set the pulse width range of your servo for PWM control of rotating 0-180 degree (min_pulse, max_pulse)
 # Each servo might be different, you can normally find this information in the servo datasheet
-servo_move.set_pulse_width_range(500, 2500)
+servo_slide_forward.set_pulse_width_range(500, 2500)
+servo_slide_backward.set_pulse_width_range(500, 2500)
 servo_pick.set_pulse_width_range(500, 2500)
 
-degree_pick = 360
 
 
-def rotate_servo(closet_distance):
-    turn = closet_distance * 10 #TODO: need to alter 10 to correct number
+def move_servo_slide(closet_distance):
+    if closet_distance > 0:
+        servo_slide = servo_slide_forward
+        print('move forward')
+    else:
+        servo_slide = servo_slide_backward
+        print('move backward')
+
+    print('move %d position'%closet_distance)
+
+    turn = abs(closet_distance * 5) #TODO: need to alter 5 to correct number
+    print('move %d turns'%turn)
 
     for i in range(turn):
         try:
             # Set the servo to 180 degree position
-            servo_move.angle = 180
+            servo_slide.angle = 180
             time.sleep(0.27) # the best sleep time
             # Set the servo to 0 degree position
-            servo_move.angle = 0
+            servo_slide.angle = 0
             time.sleep(0.27)
-
+            d
         except KeyboardInterrupt:
             # Once interrupted, set the servo back to 0 degree position
-            servo_move.angle = 0
+            servo_slide.angle = 0
             time.sleep(0.5)
 
-def choose_clothes(clothes, curr_pos):
-    target_pos = clothes[clothes]
-
-
-def push_clothes():
+def push_cloth():
+    print('push cloth')
     try:
         # Set the servo to 180 degree position
         servo_pick.angle = 180
@@ -49,14 +57,32 @@ def push_clothes():
         servo_pick.angle = 0
     except KeyboardInterrupt:
     # Once interrupted, set the servo back to 0 degree position
-        servo.angle = 0
+        servo_pick.angle = 0
         time.sleep(0.5)
 
 
-# clothes = {'coat':0,'tshirt':1}
-# curr_pos = 0
+def retrieve_cloth(curr_pos,target_cloth):
 
-rotate_servo(1)
+    closet_cloth = {'coat':0,'tshirt':1,'jacket':2}
+    target_pos = closet_cloth[target_cloth]
+    print('current position is %d, moving to position %d.'%(curr_pos,target_pos))
+
+    move_distance = curr_pos - target_pos
+    move_servo_slide(move_distance)
+
+    push_cloth()
+    curr_pos = target_pos
+
+    return curr_pos
+
+
+
+# test
+curr_pos = 0
+
+curr_pos = retrieve_cloth(curr_pos,'jacket')
+
+
 
 
 
