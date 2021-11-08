59b2703d1c7f2e706ca2dac3a4c2ec432fc8e5d4
Yuzhen Zhang <yz869@cornell.edu>
2021-11-07 19:33:40 -0500

    Update servo_adam.py

--- Lab 5/servo_adam.py
+++ Lab 5/servo_adam.py
@@ -16,12 +16,12 @@ servo_grab.set_pulse_width_range(500, 2500)
 
 def grab_cloth():
     deg = 0
-    while deg < 150:
+    while deg < 140:
         deg += 1
         try:
             # Set the servo to degree position
             servo_grab.angle = deg
-            time.sleep(0.1)
+            time.sleep(0.05)
 
         except KeyboardInterrupt:
             # Once interrupted, set the servo back to 0 degree position
@@ -30,13 +30,13 @@ def grab_cloth():
             break
 
 def release_cloth():
-    deg = 150
+    deg = 140
     while deg > 0:
         deg -= 1
         try:
             # Set the servo to degree position
             servo_grab.angle = deg
-            time.sleep(0.1)
+            time.sleep(0.05)
 
         except KeyboardInterrupt:
             # Once interrupted, set the servo back to 0 degree position
