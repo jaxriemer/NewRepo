9ee8fcbce5256b75768f288aa28bd0ab017a217c
Yuzhen Zhang <yz869@cornell.edu>
2021-11-07 20:07:01 -0500

    Update servo_test.py

--- Lab 4/servo_test.py
+++ Lab 4/servo_test.py
@@ -16,10 +16,10 @@ while True:
     try:
         # Set the servo to 180 degree position
         servo.angle = 180
-        time.sleep(2)
+        time.sleep(0.27)
         # Set the servo to 0 degree position
         servo.angle = 0
-        time.sleep(2)
+        time.sleep(0.27)
 
     except KeyboardInterrupt:
         # Once interrupted, set the servo back to 0 degree position
