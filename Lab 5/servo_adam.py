df67c14a1727a91c73ebb16dc225606eaf24d1a8
Yuzhen Zhang <yz869@cornell.edu>
2021-11-07 19:26:27 -0500

    Update servo_adam.py

--- Lab 5/servo_adam.py
+++ Lab 5/servo_adam.py
@@ -6,23 +6,27 @@ from adafruit_servokit import ServoKit
 kit = ServoKit(channels=16)
 
 # Name and set up the servo according to the channel you are using.
-servo = kit.servo[0]
+servo_grab = kit.servo[2]
 
 # Set the pulse width range of your servo for PWM control of rotating 0-180 degree (min_pulse, max_pulse)
 # Each servo might be different, you can normally find this information in the servo datasheet
 servo.set_pulse_width_range(500, 2500)
 
-while True:
-    try:
-        # Set the servo to 180 degree position
-        servo.angle = 180
-        time.sleep(0.7)
-        # Set the servo to 0 degree position
-        servo.angle = 0
-        time.sleep(0.7)
-        
-    except KeyboardInterrupt:
-        # Once interrupted, set the servo back to 0 degree position
-        servo.angle = 0
-        time.sleep(0.5)
-        break
+
+
+def grab_cloth():
+    deg = 0
+    while deg < 180:
+        deg += 1
+        try:
+            # Set the servo to degree position
+            servo.angle = deg
+            time.sleep(0.1)
+
+        except KeyboardInterrupt:
+            # Once interrupted, set the servo back to 0 degree position
+            servo.angle = 0
+            time.sleep(0.5)
+            break
+
+grab_cloth()
