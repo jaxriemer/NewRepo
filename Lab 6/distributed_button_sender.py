b71f9e9e5bd3bc59b840575ddc28a79de854a80f
XinningF <xf49@cornell.edu>
2021-11-15 16:16:02 -0500

    add distributed_button_sender.py

---
+++ Lab 6/distributed_button_sender.py
@@ -0,0 +1,28 @@
+import time
+import board
+import busio
+import qwiic_button
+
+import paho.mqtt.client as mqtt
+import uuid
+
+client = mqtt.Client(str(uuid.uuid1()))
+client.tls_set()
+client.username_pw_set('idd', 'device@theFarm')
+
+client.connect(
+    'farlab.infosci.cornell.edu',
+    port=8883)
+
+topic = 'IDD/nek/button'
+
+i2c = busio.I2C(board.SCL, board.SDA)
+button = qwiic_button.QwiicButton()
+button.begin()
+
+while True:
+    for i in range(12):
+        if button.is_button_pressed():
+        	print("button pressed")
+            client.publish(topic, val)
+    time.sleep(0.25)
