c8ca5583067a4480328a339d65a2031f6d8f0dd0
KH <liqin.he.ken@gmail.com>
2021-12-01 23:33:38 -0500

    update servo_motion.py

--- Final Project/Face/servo_motion.py
+++ Final Project/Face/servo_motion.py
@@ -15,15 +15,15 @@ topic = 'IDD/face_motion'
 eye_status = 'Not read'
 
 # # Set channels to the number of servo channels on your kit.
-# # There are 16 channels on the PCA9685 chip.
-# kit = ServoKit(channels=16)
-#
-# # Name and set up the servo according to the channel you are using.
-# servo_eye = kit.servo[0]
-#
-# # Set the pulse width range of your servo for PWM control of rotating 0-180 degree (min_pulse, max_pulse)
-# # Each servo might be different, you can normally find this information in the servo datasheet
-# servo_eye.set_pulse_width_range(500, 2500)
+# There are 16 channels on the PCA9685 chip.
+kit = ServoKit(channels=16)
+
+# Name and set up the servo according to the channel you are using.
+servo_eye = kit.servo[0]
+
+# Set the pulse width range of your servo for PWM control of rotating 0-180 degree (min_pulse, max_pulse)
+# Each servo might be different, you can normally find this information in the servo datasheet
+servo_eye.set_pulse_width_range(500, 2500)
 
 def on_connect(client, userdata, flags, rc):
 	print(f"connected with result code {rc}")
@@ -38,7 +38,7 @@ def on_message(cleint, userdata, msg):
     if msg.topic == topic:
         global eye_status
         eye_status = msg.payload.decode('UTF-8')
-        print(eye_status)
+        # print(eye_status)
 
 # Every client needs a random ID
 client = mqtt.Client(str(uuid.uuid1()))
@@ -61,6 +61,7 @@ client.connect(
 # this is blocking. to see other ways of dealing with the loop
 #  https://www.eclipse.org/paho/index.php?page=clients/python/docs/index.php#network-loop
 
+
 def eye_movement(condition):
     # check this later
     open_deg = 180
@@ -73,14 +74,13 @@ def eye_movement(condition):
         servo_eye.angle = close_deg
         time.sleep(0.03)
 
-
 client.loop_start()
 
 # TODO: retreive the eye condition on MQTT
 
 while True:
     print(eye_status)
-    # eye_movement(eye_status)
+    eye_movement(eye_status)
 
 
 
