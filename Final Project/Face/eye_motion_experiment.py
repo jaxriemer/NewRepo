23d6935f80a591a055890f1befd9549c5fa1980e
XinningF <xf49@cornell.edu>
2021-12-06 17:49:24 -0500

    Update eye_motion_experiment.py

--- Final Project/Face/eye_motion_experiment.py
+++ Final Project/Face/eye_motion_experiment.py
@@ -75,7 +75,7 @@ client.connect(
 
 def eyelid_movement(status):
     print("upper eyelid open")
-    eyelid_upper_servo.angle = 50
+    eyelid_upper_servo.angle =30
     print("lower eyelid open")
     eyelid_lower_servo.angle = 0
 
