ec8aca669c3e5575e970d4c02703242ffeb6fa38
KH <liqin.he.ken@gmail.com>
2021-12-06 23:03:53 -0500

    update model

--- Final Project/Canvas/servo_test.py
+++ Final Project/Canvas/servo_test.py
@@ -18,7 +18,7 @@ eyebrow_servo_servo = kit.servo[3]
 while True:
     try:
         # Set the servo to 180 degree position
-        eyebrow_servo_servo.angle = 35
+        eyebrow_servo_servo.angle = 30
         time.sleep(3)
         # Set the servo to 0 degree position
         eyebrow_servo_servo.angle = 0
