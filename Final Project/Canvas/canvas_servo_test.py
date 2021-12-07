602457eea3b2efced5af16b4bf0d0f2ade56b819
KH <liqin.he.ken@gmail.com>
2021-12-07 02:03:14 -0500

    update model

--- Final Project/Canvas/canvas_servo_test.py
+++ Final Project/Canvas/canvas_servo_test.py
@@ -129,17 +129,17 @@ while True:
         #     time.sleep(0.05)
     try:
         # Set the servo to 180 degree position
-        print('pushing out')
-        while servo_upper_0.angle > outside:
-            servo_upper_0.angle += show_add
-            time.sleep(0.02)
-            print(servo_upper_0.angle)
-
-        print('pulling in')
-        while servo_upper_0.angle < inside:
-            servo_upper_0.angle += no_show_add
-            time.sleep(0.02)
-            print(servo_upper_0.angle)
+        # print('pushing out')
+        # while servo_upper_0.angle > outside:
+        #     servo_upper_0.angle += show_add
+        #     time.sleep(0.02)
+        #     print(servo_upper_0.angle)
+        #
+        # print('pulling in')
+        # while servo_upper_0.angle < inside:
+        #     servo_upper_0.angle += no_show_add
+        #     time.sleep(0.02)
+        #     print(servo_upper_0.angle)
 
         while servo_bottom_0.angle > outside:
             servo_bottom_0.angle += show_add
@@ -152,21 +152,23 @@ while True:
             time.sleep(0.02)
             print(servo_bottom_0.angle)
 
-        while servo_upper_1.angle > outside:
-            servo_upper_1.angle += show_add
-            time.sleep(0.02)
-            print(servo_upper_1.angle)
-
-        print('pulling in')
-        while servo_upper_1.angle < inside:
-            servo_upper_1.angle += no_show_add
-            time.sleep(0.02)
-            print(servo_upper_1.angle)
+        # while servo_upper_1.angle > outside:
+        #     servo_upper_1.angle += show_add
+        #     time.sleep(0.02)
+        #     print(servo_upper_1.angle)
+        #
+        # print('pulling in')
+        # while servo_upper_1.angle < inside:
+        #     servo_upper_1.angle += no_show_add
+        #     time.sleep(0.02)
+        #     print(servo_upper_1.angle)
 
     except KeyboardInterrupt:
         # Once interrupted, set the servo back to 0 degree position
 
-        servo_upper_0.angle = 0
+        servo_upper_0.angle = 180
+        servo_bottom_0.angle = 180
+        servo_upper_1 = 180
         time.sleep(1)
         break
 
