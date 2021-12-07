18f1a4ca82bfdee1f998b59815ba879f94ab4c96
XinningF <xf49@cornell.edu>
2021-12-07 05:35:43 -0500

    Update servo_test.py

--- Final Project/Canvas/servo_test.py
+++ Final Project/Canvas/servo_test.py
@@ -22,6 +22,10 @@ while True:
         eyelid_upper_servo.angle = 3
         eyelid_lower_servo.angle = 80
         time.sleep(3)
+
+        eyelid_upper_servo.angle = 30
+        eyelid_lower_servo.angle = 80
+        time.sleep(3)
         # eye_horizontal_servo.angle = 39
         # time.sleep(3)
         # eye_horizontal_servo.angle = 0
