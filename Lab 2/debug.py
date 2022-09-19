0de3e8fd88eb23c1fd48701bfd59baacb837300c
Jackson Riemerschmid <jacksonriemerschmid@gmail.com>
2022-09-19 15:29:16 -0400

    .

--- Lab 2/debug.py
+++ Lab 2/debug.py
@@ -80,89 +80,146 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(START_BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(BREAK_BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-previous_break_state = GPIO.input(BREAK_BUTTON_PIN)
-previous_start_state = GPIO.input(START_BUTTON_PIN)
+#CREATE COUNTDOWN
+rotation = 90
+def countdown(t):
+   print('Start of countdown loop')
+   # print(continue_countdown)
+   while (t and GPIO.input(BREAK_BUTTON_PIN) != GPIO.LOW):
+      mins, secs = divmod(t, 60)
+      timer = '{:02d}:{:02d}'.format(mins, secs)
+      print(timer, end="\r")
+      time.sleep(1)
+      t -= 1  
+      if GPIO.input(BREAK_BUTTON_PIN) == GPIO.LOW:
+         # continue_countdown = False
+         print('Stop countdown')
+         return
 
-print(previous_break_state)
-print(previous_start_state)
-# def countdown_image_sequence():
-#    print('Button is pressed')
-#    countdown(int(5))
-#    draw = ImageDraw.Draw(img)
-#    draw.rectangle(
-#    (4,236,25,204),
-#    fill = (125, 125, 125),
-#    outline = (0, 0, 0))
-#    disp.image(img)
+   # if continue_countdown == False:
+   #    print('Exit function')
+   #    # print(continue_countdown)
+   #    return
+# input time in seconds
+# t = 2 
+# function call
+# countdown(int(t))
 
-#    countdown(int(5))
-#    draw = ImageDraw.Draw(img)
-#    draw.rectangle(
-#    (4,236,50,204),
-#    fill = (125, 125, 125),
-#    outline = (0, 0, 0))
-#    disp.image(img)
 
-#    countdown(int(5))
-#    draw = ImageDraw.Draw(img)
-#    draw.rectangle(
-#    (4,236,75,204),
-#    fill = (125, 125, 125),
-#    outline = (0, 0, 0))
-#    disp.image(img)
-
-#    countdown(int(5))
-#    draw = ImageDraw.Draw(img)
-#    draw.rectangle(
-#    (4,236,100,204),
-#    fill = (125, 125, 125),
-#    outline = (0, 0, 0))
-#    disp.image(img)
-
-#    countdown(int(5))
-#    draw = ImageDraw.Draw(img)
-#    draw.rectangle(
-#    (4,236,125,204),
-#    fill = (125, 125, 125),
-#    outline = (0, 0, 0))
-#    disp.image(img)
-
-#    countdown(int(5))
-#    draw = ImageDraw.Draw(img)
-#    draw.rectangle(
-#    (4,236,150,204),
-#    fill = (125, 125, 125),
-#    outline = (0, 0, 0))
-#    disp.image(img)
 
 try:
-#    while True:
-#       if GPIO.input(START_BUTTON_PIN) == GPIO.LOW: #Check if user has started countdown
-#          countdown_image_sequence()
    while True:
-        current_break_state = GPIO.input(BREAK_BUTTON_PIN)
-        current_start_state = GPIO.input(START_BUTTON_PIN)
-        print('Start')
-        
-        if (current_break_state == previous_break_state and current_start_state != previous_start_state):
-            #CREATE COUNTDOWN
-            rotation = 90
-            print('Start of countdown loop')
-            t = 3
-            mins, secs = divmod(t, 60)
-            timer = '{:02d}:{:02d}'.format(mins, secs)
-            print(timer, end="\r")
-            time.sleep(1)
-            t -= 1
+      if GPIO.input(START_BUTTON_PIN) == GPIO.LOW: #Check if user has started countdown
+        image = Image.new("RGB", (width, height))
 
-            previous_break_state = current_break_state
-            current_break_state = GPIO.input(BREAK_BUTTON_PIN)
-            previous_start_state = current_start_state
-            current_start_state = GPIO.input(START_BUTTON_PIN)
-            # if GPIO.input(BREAK_BUTTON_PIN) == GPIO.LOW:
-            #     print('Stop countdown')
-            # else:
-            #     print('Button is not pressed')
+        # Get drawing object to draw on image.
+        draw = ImageDraw.Draw(image)
+        # Draw a black filled box to clear the image.
+        draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
+        disp.image(image)
+
+        # creating new Image object
+        img = Image.new('RGB', (135, 240), (125, 125, 125))
+        draw = ImageDraw.Draw(img)
+
+        draw.rectangle(
+        (0,240,240,200),
+        fill = (0,0,255),
+        outline=(0, 0, 0))
+
+
+        draw = ImageDraw.Draw(img)
+
+        draw.rectangle(
+        (4,236,132,204),
+        fill=(255, 0, 0),
+        outline=(0, 0, 0))
+
+        button_count = 0
+        disp.image(img)
+        if GPIO.input(BREAK_BUTTON_PIN) == GPIO.HIGH:
+            draw.rectangle(
+            (4,236,132,204),
+            fill=(255, 0, 0),
+            outline=(0, 0, 0))
+            disp.image(img)
+
+            image = Image.open("smiley2.jpg")
+            disp.image(img)
+            
+            print('Button is pressed')
+            countdown(int(5))
+            if GPIO.input(BREAK_BUTTON_PIN) == GPIO.HIGH:
+                draw = ImageDraw.Draw(img)
+                draw.rectangle(
+                (4,236,25,204),
+                fill = (125, 125, 125),
+                outline = (0, 0, 0))
+                disp.image(img)
+
+            countdown(int(5))
+            if GPIO.input(BREAK_BUTTON_PIN) == GPIO.HIGH:
+                draw = ImageDraw.Draw(img)
+                draw.rectangle(
+                (4,236,50,204),
+                fill = (125, 125, 125),
+                outline = (0, 0, 0))
+                disp.image(img)
+
+            countdown(int(5))
+            if GPIO.input(BREAK_BUTTON_PIN) == GPIO.HIGH:  
+                draw = ImageDraw.Draw(img)
+                draw.rectangle(
+                (4,236,75,204),
+                fill = (125, 125, 125),
+                outline = (0, 0, 0))
+                disp.image(img)
+        
+            countdown(int(5))
+            if GPIO.input(BREAK_BUTTON_PIN) == GPIO.HIGH:
+                draw = ImageDraw.Draw(img)
+                draw.rectangle(
+                (4,236,100,204),
+                fill = (125, 125, 125),
+                outline = (0, 0, 0))
+                disp.image(img)
+
+            countdown(int(5))
+            if GPIO.input(BREAK_BUTTON_PIN) == GPIO.HIGH:
+                draw = ImageDraw.Draw(img)
+                draw.rectangle(
+                (4,236,125,204),
+                fill = (125, 125, 125),
+                outline = (0, 0, 0))
+                disp.image(img)
+
+            countdown(int(5))
+            if GPIO.input(BREAK_BUTTON_PIN) == GPIO.HIGH:
+                draw = ImageDraw.Draw(img)
+                draw.rectangle(
+                (4,236,150,204),
+                fill = (125, 125, 125),
+                outline = (0, 0, 0))
+                disp.image(img)
+            if GPIO.input(BREAK_BUTTON_PIN) == GPIO.HIGH:
+                print('STOP')
+                # Draw a black filled box to clear the image.
+                draw.rectangle((0, 0, width, height), outline=0, fill=0)
+                disp.image(img)
+
+                # Load a TTF Font
+                font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf')
+                # Draw Some Text
+                text = "Take a Break!"
+                (font_width, font_height) = font.getsize(text)
+                draw.text((width//2 - font_width//2, height//2 - font_height//2),
+                text, font=font, fill=(255, 255, 0))
+                img_rotate = img.rotate(90)
+                disp.image(img_rotate)
+        else:
+            break
+   else:
+      print('Button is not pressed')
 
 except KeyboardInterrupt:
    GPIO.cleanup()
